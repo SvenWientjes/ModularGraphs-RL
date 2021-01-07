c5fde2d879653de1e5b8eb1307fe1171449df6fb
Sven <wientjes.s@gmail.com>
2021-01-07 20:41:43 +0100

    Init policy.generate

---
+++ Rsims/src/policy_generate.R
@@ -0,0 +1,6 @@
+# Function that can take a hitMat and the full list of edges mapped to identities in the hitMat
+#   Returns the optimal stopping step for each vertex or transition (in nBT)
+
+policy.generate.nBT <- function(Edges, hitMat, idmap){
+  
+}
