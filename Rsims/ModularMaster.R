053a568c7e64366c62311532e461c3eb7b1e3b96
Sven <wientjes.s@gmail.com>
2021-01-10 16:15:26 +0100

    Add new optimal stopper & finish nBT modularmaster
    Removed old Schapiro optimal stopping agent and moved old run scripts into outdated folder

--- Rsims/ModularMaster.R
+++ Rsims/ModularMaster.R
@@ -16,11 +16,16 @@ sapply(paste0('src/',list.files('src/')), source)
 # Get different Defining Parameters
 vStart <- 1    # Nr of starting node
 vGoal  <- 8    # Nr of goal (terminating, rewarding) node
-nSteps <- 9   # Nr of maximum steps in a miniblock
+nSteps <- 10   # Nr of maximum steps in a miniblock (hitMat and EVcalc will use nSteps-1; agent simulations will use nSteps!)
 gRew   <- 7    # Reward upon reaching vGoal
-sCost  <- 0.15 # Points detracted from accumulated reward for each taken step
+sCost  <- 0.31 # Points detracted from accumulated reward for each taken step
 
-Edges <- list(c(2, 3, 4, 5), #Full Schapiro-style edge matrix
+# Get parameters for agentic simulations
+nPP <- 250
+nTr <- 100
+
+#Full Schapiro-style edge matrix
+Edges <- list(c(2, 3, 4, 5), 
               c(1, 3, 4, 5),
               c(1, 2, 4, 5),
               c(1, 2, 3, 6),
@@ -37,6 +42,7 @@ Edges <- list(c(2, 3, 4, 5), #Full Schapiro-style edge matrix
               c(5, 12,13,14))
 
 ############# No-BackTracking Schapiro Analysis ----------------------------
+    # Note: should in principle work for ANY graph which has an equal degree for each node!
 inspect.Edges <- rbind(c(1,2), #List all pre- and current vertices to inspect
                        c(1,4),
                        c(1,5),
@@ -52,18 +58,44 @@ idmap <- list(a = c(1,2,3, 12,13,14), b = c(4,11), c = c(5,15), d = c(6,10), e =
 
 # Get the hitMat
 hitMat.nBT <- foreach(preV = inspect.Edges[,1], curV = inspect.Edges[,2], .combine=rbind) %do% {
-  tempMat <- hitMat.calc.nBT(Edges=Edges, vGoal=vGoal, nSteps=nSteps, curV=curV, preV=preV, totP=3^nSteps)
+  tempMat <- hitMat.calc.nBT(Edges=Edges, vGoal=vGoal, nSteps=nSteps-1, curV=curV, preV=preV, totP=3^nSteps)
   tempMat
 }
 
 # Get the Expected Values for each interaction of previous & current node, conditional upon steps left
 EVmat.nBT <- foreach(preV = inspect.Edges[,1], curV = inspect.Edges[,2], .combine=rbind) %do% {
-  tempMat <- EVcalc.nBT(Edges=Edges, curV=curV, preV=preV, vGoal=vGoal, nSteps=nSteps, gRew=gRew, sCost=sCost, hitMat=hitMat.nBT) 
+  tempMat <- EVcalc.nBT(Edges=Edges, curV=curV, preV=preV, vGoal=vGoal, nSteps=nSteps-1, gRew=gRew, sCost=sCost, hitMat=hitMat.nBT) 
   tempMat
 }
 
 # Get the optimal stopping index (equals or lower) for each possible transition
 piMat.nBT <- policy.generate.nBT(Edges=Edges, EVmat=EVmat.nBT, idmap=idmap)
+
+# Run several agents on this task, all encompassing different heuristics
+AG.dat <- data.frame(pp=0, trial=0, trRew=0, nSteps=0, endV=0, totRew=0, strat='init')
+for(pp in 1:nPP){
+  AG.dat <- rbind(AG.dat, RandomStopper.nBT( Edges=Edges, vStart=vStart, vGoal=vGoal, nSteps=nSteps, gRew=gRew, sCost=sCost, nTrials=nTr, parNum=pp, startRew=0))
+  AG.dat <- rbind(AG.dat, LazyWaiter.nBT(    Edges=Edges, vStart=vStart, vGoal=vGoal, nSteps=nSteps, gRew=gRew, sCost=sCost, nTrials=nTr, parNum=pp, startRew=0))
+  AG.dat <- rbind(AG.dat, RandomLengther.nBT(Edges=Edges, vStart=vStart, vGoal=vGoal, nSteps=nSteps, gRew=gRew, sCost=sCost, nTrials=nTr, parNum=pp, startRew=0))
+  AG.dat <- rbind(AG.dat, OptimalStopper.nBT(Edges=Edges, vStart=vStart, vGoal=vGoal, nSteps=nSteps, gRew=gRew, sCost=sCost, nTrials=nTr, parNum=pp, startRew=0, piMat=piMat.nBT))
+}
+AG.dat <- AG.dat[-1,]
+AG.dat$strat <- droplevels(AG.dat$strat)
+
+# Summarize agent simulations per participant
+AG.dat.ppEval <- data.frame(pp=0, totRew=0, endV.p=0, strat='init')
+for(pp in 1:nPP){
+  for(strat in levels(AG.dat$strat)){
+    totRew <- AG.dat[AG.dat$pp==pp & AG.dat$trial==max(AG.dat$trial) & AG.dat$strat==strat,]$totRew
+    endV.p <- sum(AG.dat[AG.dat$pp==pp & AG.dat$strat==strat,]$endV==vGoal)
+    AG.dat.ppEval <- rbind(AG.dat.ppEval, data.frame(pp=pp, totRew=totRew, endV.p=endV.p, strat=strat))
+  }
+}
+AG.dat.ppEval <- AG.dat.ppEval[-1,]
+
+# Plot agent data!
+ggplot(AG.dat.ppEval, aes(x=totRew, col=strat)) +
+  geom_density()
 
 ############# BackTracking Schapiro Analysis ----------------------------
 'TBA'
--- Rsims/src/Agents.R
+++ Rsims/src/Agents.R
@@ -77,65 +77,29 @@ RandomLengther.nBT <- function(Edges, vStart, vGoal, nSteps, gRew, sCost, nTrial
   return(parDat[-1,])
 }
 
-OptimalStopper.Schapiro.nBT <- function(Edges, vStart, vGoal, nSteps, gRew, sCost, nTrials, hitMat, goalstepchance, parNum=1, startRew=0){
+OptimalStopper.nBT <- function(Edges, vStart, vGoal, nSteps, gRew, sCost, nTrials, piMat, parNum=1, startRew=0){
   parDat <- data.frame(pp=parNum, trial=0, trRew=0, nSteps=0, endV=0, totRew=0, strat='init')
-  
-  # Get the points where EV becomes negative - now we want to stop!
-  EV_data <- EVcalc(Edges = Edges, vStart=vStart, vGoal=vGoal, nSteps=nSteps, gRew=gRew, sCost=sCost, hitMat=hitMat, goalstepchance=goalstepchance)
-  changePoint <- data.frame(trans=c('1<-4','1<-2','1<-5','6<-4', '7<-6', '7<-9', '6<-7', '4<-1','5<-15','5<-1','4<-6'), cP=0)
-  
-  for(tr in levels(changePoint$trans)){
-    if(T %in% (EV_data[EV_data$trans==tr,'value']<=0)){
-      #changePoint <- rbind(changePoint, data.frame(trans=tr, cP=max(which(EV_data[EV_data$trans==tr,'value']<=0))))
-      changePoint[changePoint$trans==tr,]$cP <- max(which(EV_data[EV_data$trans==tr,'value']<=0))
-    }
-  }
-  
   totRew <- startRew
   for(tr in 1:nTrials){
-    path <- c(vStart, sample(Edges[[vStart]],1))
-    totRew <- totRew-sCost                         #Pay the first cost
+    path   <- c(vStart, sample(Edges[[vStart]],1)) #First step (forced)
+    totRew <- totRew-sCost
     trRew  <- -sCost
-    
     for(st in 2:(nSteps+1)){
-      if(tail(path,1)==vGoal){ #If you reach the goal
+      if(tail(path,1)==vGoal){
         totRew <- totRew+gRew
         parDat <- rbind(parDat, data.frame(pp=parNum, trial=tr, trRew=(trRew+gRew), nSteps=(length(path)-1), endV=tail(path,1), totRew=totRew, strat='OS'))
         break
-      }else if(st==(nSteps+1)){ #If you reached the maximum number of steps in a trial
-        parDat <- rbind(parDat, data.frame(pp=parNum, trial=tr, trRew=trRew, nSteps=(length(path)-1), endV=tail(path,1), totRew=totRew, strat='OS'))
-        break
-        # CHECK ALL CLASSES for optimal stopping
-      }else if(tail(path,1) %in% c( 1, 2, 3) & tail(path,2)[1] %in% c( 4)       & (nSteps-st+1) <= changePoint[changePoint$trans=='1<-4',]$cP |
-               tail(path,1) %in% c(12,13,14) & tail(path,2)[1] %in% c(11)       & (nSteps-st+1) <= changePoint[changePoint$trans=='1<-4',]$cP |
-               tail(path,1) %in% c(12,13,14) & tail(path,2)[1] %in% c(12,13,14) & (nSteps-st+1) <= changePoint[changePoint$trans=='1<-2',]$cP |
-               tail(path,1) %in% c( 1, 2, 3) & tail(path,2)[1] %in% c( 1, 2, 3) & (nSteps-st+1) <= changePoint[changePoint$trans=='1<-2',]$cP |
-               tail(path,1) %in% c( 1, 2, 3) & tail(path,2)[1] %in% c( 5)       & (nSteps-st+1) <= changePoint[changePoint$trans=='1<-5',]$cP |
-               tail(path,1) %in% c(12,13,14) & tail(path,2)[1] %in% c(15)       & (nSteps-st+1) <= changePoint[changePoint$trans=='1<-5',]$cP |
-               tail(path,1) %in% c( 6,10)    & tail(path,2)[1] %in% c( 4,11)    & (nSteps-st+1) <= changePoint[changePoint$trans=='6<-4',]$cP |
-               tail(path,1) %in% c( 7, 9)    & tail(path,2)[1] %in% c( 6,10)    & (nSteps-st+1) <= changePoint[changePoint$trans=='7<-6',]$cP |
-               tail(path,1) %in% c( 7, 9)    & tail(path,2)[1] %in% c( 7, 9)    & (nSteps-st+1) <= changePoint[changePoint$trans=='7<-9',]$cP |
-               tail(path,1) %in% c( 6,10)    & tail(path,2)[1] %in% c( 7, 9)    & (nSteps-st+1) <= changePoint[changePoint$trans=='6<-7',]$cP |
-               tail(path,1) %in% c( 4)       & tail(path,2)[1] %in% c( 1, 2, 3) & (nSteps-st+1) <= changePoint[changePoint$trans=='4<-1',]$cP |
-               tail(path,1) %in% c(11)       & tail(path,2)[1] %in% c(12,13,14) & (nSteps-st+1) <= changePoint[changePoint$trans=='4<-1',]$cP |
-               tail(path,1) %in% c( 5,15)    & tail(path,2)[1] %in% c( 5,15)    & (nSteps-st+1) <= changePoint[changePoint$trans=='5<-15',]$cP|
-               tail(path,1) %in% c( 5)       & tail(path,2)[1] %in% c( 1, 2, 3) & (nSteps-st+1) <= changePoint[changePoint$trans=='5<-1',]$cP |
-               tail(path,1) %in% c(15)       & tail(path,2)[1] %in% c(12,13,14) & (nSteps-st+1) <= changePoint[changePoint$trans=='5<-1',]$cP |
-               tail(path,1) %in% c( 4,11)    & tail(path,2)[1] %in% c( 6,10)    & (nSteps-st+1) <= changePoint[changePoint$trans=='4<-6',]$cP){ 
+      }else if((nSteps - length(path)+1) <= piMat[which(as.logical(apply(piMat[,c(1,2)], 1, all.equal, current=tail(path,2), check.attributes=F))),3]){
         parDat <- rbind(parDat, data.frame(pp=parNum, trial=tr, trRew=trRew, nSteps=(length(path)-1), endV=tail(path,1), totRew=totRew, strat='OS'))
         break
       }else{
         path <- c(path, sample(Edges[[tail(path,1)]][-which(Edges[[tail(path,1)]]==tail(path,2)[1])],1))
-        totRew <- totRew-sCost
+        totRew <- totRew- sCost
         trRew  <- trRew - sCost
       }
     }
   }
   return(parDat[-1,])
-}
-
-OptimalStopper.nBT <- function(Edges, Etc){
-  
 }
 
 ModularStopper <- function(Edges, vStart, vGoal, nSteps, gRew, sCost, nTrials, modTrans, parNum=1, startRew=0){
