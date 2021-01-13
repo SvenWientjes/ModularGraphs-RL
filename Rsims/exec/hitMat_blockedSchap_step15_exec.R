05bef7bfe51328494e4c9172e33c2c6bc488dd94
Sven <wientjes.s@gmail.com>
2021-01-13 11:33:16 +0100

    Updated blockedSchap exec for testing

--- Rsims/exec/hitMat_blockedSchap_step15_exec.R
+++ Rsims/exec/hitMat_blockedSchap_step15_exec.R
@@ -30,6 +30,7 @@ Edges <- list(c(2, 3, 4, 5),
               c(5, 12,13,14))
 
 inspect.Vertices <- c(1,4,5,6,7,10,11,12,15)
+inspect.Vertices <- c(1)
 
 # Set up parallel cluster
 cl <- makeCluster(5)
