b1f0dd794d6aa7ad70a2ba6940066e31298487cd
Sven <wientjes.s@gmail.com>
2021-03-01 21:55:10 +0100

    Get betting agents

--- Rsims/src/Bet_functions.R
+++ Rsims/src/Bet_functions.R
@@ -46,8 +46,46 @@ Bet.gen <- function(Edges, nVisit, nTr, c.map, idmap.g, bt.map, parNum=1){
   return(experiment[-1,])
 }
 
+bet.sum <- function(vec, startP){
+  cum <- rep(0, length(vec))
+  cum[1] <- startP+vec[1]
+  for(i in 2:length(vec)){
+    cum[i] <- max(0, cum[i-1]+vec[i])
+  }
+  return(cum)
+}
 
-
-
-
-
+policyBet <- function(curV, goalV, goal.c, idmap.g, bt.map, c.map, idmap.d, idmap.bg, stopIdx){
+  
+  if(goalV %in% unlist(bt.map)){
+    close.c <- c.map[[goal.c]][bt.map[[goal.c]]==goalV]
+    far.c   <- c.map[[goal.c]][bt.map[[goal.c]]!=goalV]
+    leave.c <- c('a','b','c','d')[!c('a','b','c','d') %in% c(goal.c,close.c,far.c)]
+    
+    newPol <- list(a = bt.map[[close.c]][c.map[[close.c]]!=goal.c],
+                   b = idmap.g[[close.c]],
+                   c = bt.map[[close.c]][c.map[[close.c]]==goal.c],
+                   d = idmap.g[[goal.c]],
+                   e = bt.map[[goal.c]][c.map[[goal.c]]==far.c],
+                   f = bt.map[[far.c]][c.map[[far.c]]==goal.c],
+                   g = idmap.g[[far.c]],
+                   h = bt.map[[far.c]][c.map[[far.c]]!=goal.c],
+                   i = bt.map[[leave.c]][c.map[[leave.c]]==far.c],
+                   j = idmap.g[[leave.c]],
+                   k = bt.map[[leave.c]][c.map[[leave.c]]==close.c]
+    )
+    return(stopIdx[vertex %in% idmap.bg[[names(which(sapply(newPol, function(m){curV %in% m})))]],V1])
+  }else{
+    lat.c <- names(which(sapply(c.map, function(m){goal.c %in% m})))
+    leave.c <- c('a','b','c','d')[!c('a','b','c','d') %in% c(goal.c,lat.c)]
+    
+    newPol <- list(a = sapply(lat.c, function(m){bt.map[[m]][c.map[[m]]!=goal.c]}),
+                   b = unlist(idmap.g[lat.c]),
+                   c = sapply(lat.c, function(m){bt.map[[m]][c.map[[m]]==goal.c]}),
+                   d = bt.map[[goal.c]],
+                   e = idmap.g[[goal.c]][idmap.g[[goal.c]]!=goalV],
+                   f = bt.map[[leave.c]],
+                   g = idmap.g[[leave.c]])
+    return(stopIdx[vertex %in% idmap.d[[names(which(sapply(newPol, function(m){curV %in% m})))]],V1])
+  }
+}
--- Rsims/tests/QuadMod_bet_master.R
+++ Rsims/tests/QuadMod_bet_master.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(reshape2)
 library(data.table)
 library(zoo)
+library(rstan)
 
 # Load Functions from /src/
 sapply(paste0('src/',list.files('src/')), source)
@@ -15,6 +16,7 @@ winM   <- 5
 startP <- 5
 nVisit <- 15
 nTr    <- 100
+nPP    <- 500
 
 #Quad Schapiro-style edge matrix
 Edges <- list(c(2, 3, 4, 20),
@@ -79,18 +81,87 @@ stopIdx.b <- EVmat.b[, EV<0,by=.(vertex,steps)
                      ]
 
 ## Get simulated trajectories ----
-# Initialize experimental experience
-full.exp <- Bet.gen(Edges, nVisit, nTr, c.map, idmap.g, bt.map)
-
-# Define random strategy
-full.exp[v!=goal, rand.choice:=sample(c(-1,1),1), by=.(tr,stepsleft)         #Sample random choice increments
-         ][,rand.trBet := na.locf(cumsum(rand.choice))+startP,by=tr          #Add up to get bets, with 5 starting points
-           ][rand.trBet<0, rand.trBet:=0                                     #Cannot bet below zero
+full.exp <- foreach(p = 1:nPP, .combine=rbind) %do% {
+  # Initialize experimental experience
+  full.exp <- Bet.gen(Edges, nVisit, nTr, c.map, idmap.g, bt.map, parNum=p)
+  
+  # Get cluster info
+  full.exp[, goal.c:=substring(names(which(sapply(unlist(c(idmap.g, bt.map), use.names=T), function(m){goal[1] %in% m}))),1,1), by=tr
+           ][, start.c:=substring(names(which(sapply(unlist(c(idmap.g, bt.map), use.names=T), function(m){v[1] %in% m}))),1,1), by=tr]
+  
+  # Define random strategy
+  full.exp[v!=goal, rand.choice:=sample(c(-1,1),1), by=.(tr,stepsleft)       #Sample random choice increments
+           ][,rand.trBet := na.locf(bet.sum(rand.choice,startP)),by=tr       #Add up to get bets, with startP starting points
              ][stepsleft == 0, rand.trRew := -rand.trBet, by=tr              #Determine losing bets
                ][v==goal, rand.trRew := winM*rand.trBet, by=tr               #Determine winning bets
                  ][!is.na(rand.trRew),rand.totRew := cumsum(rand.trRew)]     #Add up all losses and wins
+  
+  # Define modular strategy
+  full.exp[v!=goal, mod.choice:=if(v %in% unlist(c(idmap.g[c(goal.c,start.c)], bt.map[c(goal.c,start.c)]))){1}else{-1}, by=.(tr,stepsleft)  # Determine modular choice (start+goal)
+           ][,mod.trBet := na.locf(bet.sum(mod.choice, startP)),by=tr                                                                       # Add up to get bets, with startP starting points
+             ][stepsleft == 0, mod.trRew := -mod.trBet, by=tr                                                                               # Determine losing bets
+               ][v==goal, mod.trRew := winM*mod.trBet, by=tr                                                                                # Determine winning bets
+                 ][!is.na(mod.trRew),mod.totRew := cumsum(mod.trRew)]                                                                       # Add up all losses and wins
+  
+  # Define optimal strategy
+  full.exp[v!=goal, opt.choice := if(trtype == 'deep'){
+    if(stepsleft > policyBet(v, goal, goal.c, idmap.g,bt.map,c.map,idmap.d,idmap.bg,stopIdx.d)){
+      1
+    }else{
+      -1
+    }
+  }else{
+    if(stepsleft > policyBet(v, goal, goal.c, idmap.g,bt.map,c.map,idmap.d,idmap.bg,stopIdx.b)){
+      1
+    }else{
+      -1
+    }
+  }, by = .(tr,stepsleft)
+  ][,opt.trBet := na.locf(bet.sum(opt.choice, startP)),by=tr      # Add up to get bets, with startP starting points
+    ][stepsleft == 0, opt.trRew := -opt.trBet, by=tr              # Determine losing bets
+      ][v==goal, opt.trRew := winM*opt.trBet, by=tr               # Determine winning bets
+        ][!is.na(opt.trRew),opt.totRew := cumsum(opt.trRew)]      # Add up all losses and wins
+  
+  return(full.exp)
+}
 
-# Define modular strategy
+# Plot final rewards with different strategies
+ggplot(data=full.exp[tr==100 & !is.na(opt.totRew)]) +
+  geom_density(aes(x=opt.totRew), col='green') + 
+  geom_density(aes(x=mod.totRew), col='blue') +
+  geom_density(aes(x=rand.totRew), col='red')
+
+# Plot nr of betting choices made
+ggplot() +
+  geom_density(data=full.exp[,.SD[opt.choice==1,.N]/.SD[,.N],by=pp], aes(x=V1), col='green') +
+  geom_density(data=full.exp[,.SD[mod.choice==1,.N]/.SD[,.N],by=pp], aes(x=V1), col='blue') +
+  geom_density(data=full.exp[,.SD[rand.choice==1,.N]/.SD[,.N],by=pp], aes(x=V1), col='red')
+
+# Get leading nv -> use for tracking transitions
+full.exp[,nv:=shift(v,1,type='lead'), by=.(pp,tr)]
+
+# Get distribution of total edge visits
+ggplot(full.exp[!is.na(nv), .N, .(v,nv,pp)][,sum(N),by=pp]) +
+  geom_density(aes(x=V1))
+
+# Get nr of visits per unique edge per pp
+ggplot(full.exp[!is.na(nv), .N, .(v,nv,pp)]) +
+  geom_line(aes(x=interaction(v,nv), y=N, group=pp, col=pp))
+
+## Simulate probit regressions on normative choices with set noise level ----
+noiseL <- 0.2 # proportion of non-normative choices
+
+# Get non-normative choices
+full.exp[!is.na(opt.choice), c('mod.choice.e','opt.choice.e'):=list(sample(c(mod.choice,-mod.choice),size=1,prob=c(1-noiseL,noiseL)),
+                                                                    sample(c(opt.choice,-opt.choice),size=1,prob=c(1-noiseL,noiseL))
+                                                                ), by=.(pp,tr,stepsleft)]
+
+# stan?
+
+
+
+
+
 
 
 
