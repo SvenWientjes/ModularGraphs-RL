1eaa16a2502773d937ff6cbd2a2a43b0db835f29
Sven <wientjes.s@gmail.com>
2021-04-04 19:46:53 +0200

    Add choices based on SREV

--- Rsims/tests/QuadMod_bet_SREV.R
+++ Rsims/tests/QuadMod_bet_SREV.R
@@ -27,12 +27,22 @@ heatmap(SR, Rowv = NA, Colv = NA)
 heatmap(eigen(SR)$vectors[,1:7], Rowv=NA, Colv=NA)
 
 # Make choice predictions?
-ct <- 0.25
-vt <- rep(-(ct^6), 20); vt[8] <- winM
+ct <- 0.60
+vt <- rep(-(ct^3), 20); vt[8] <- winM
 SR %*% vt
 
-full.exp[pp==1]
-
-plot(seq.SR.EV(full.exp[pp==1,tr], full.exp[pp==1,v], full.exp[pp==1,goal], full.exp[pp==1,stepsleft], lr, gamm, ct, winM),
+# Plot SREV over time
+SR.EVreg <- seq.SR.EV(full.exp[pp==1,tr], full.exp[pp==1,v], full.exp[pp==1,goal], full.exp[pp==1,stepsleft], lr, gamm, ct, winM)
+plot(SR.EVreg,
      type='l')
 abline(0, b=0, col='red')
+
+# Get SREV based choices! 
+choiceS <- sapply(boot::inv.logit(2*SR.EVreg), function(b){rbinom(1,1,b)})
+plot.dat <- data.table(sym.id = full.exp[pp==1,sym.id], pol.type = full.exp[pp==1, if(trtype=='deep'){'deep'}else{'bottleneck'}, by=.(tr,stepsleft)]$V1,
+                       stepsleft = full.exp[pp==1,stepsleft], choiceS=choiceS)
+
+ggplot(plot.dat[,sum(choiceS==1)/.N, by=.(sym.id,pol.type,stepsleft)]) +
+  geom_line(aes(x=stepsleft, y=V1)) +
+  geom_point(aes(x=stepsleft, y=V1)) +
+  facet_grid(pol.type~sym.id)
