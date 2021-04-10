6b74f86f5a5d84ef8c6f2fb31420734661cab824
Sven <wientjes.s@gmail.com>
2021-04-10 17:00:45 +0200

    Add trajectory selection scripts and analysis with SR/Lynn

--- Rsims/src/Bet_functions.R
+++ Rsims/src/Bet_functions.R
@@ -148,7 +148,7 @@ seq.SR.EV <- function(tr, v, goal, stepsleft, lr, gamm, ct, winM){
     # Store current trial state sequence
     episode <- v[tr==trial]
     # Get first state EV
-    vt <- SR %*% rep(-(ct^stepsleft[tridx]), 20); vt[goal[tridx]] <- winM
+    vt <- rep(-(ct^stepsleft[tridx]), 20); vt[goal[tridx]] <- winM
     SR.EV[tridx] <- (SR %*% vt)[episode[1]]
     tridx <- tridx+1
     for(experience in 2:length(episode)){
@@ -156,10 +156,81 @@ seq.SR.EV <- function(tr, v, goal, stepsleft, lr, gamm, ct, winM){
       er <- I + gamm * SR[episode[experience],] - SR[episode[experience-1],] # Get error of t-1 SR vs t SR
       SR[episode[experience-1],] = SR[episode[experience-1],] + lr*er # Update t-1 SR
       # Perform EV
-      vt <- SR %*% rep(-(ct^stepsleft[tridx]), 20); vt[goal[tridx]] <- winM
+      vt <- rep(-(ct^stepsleft[tridx]), 20); vt[goal[tridx]] <- winM
       SR.EV[tridx] <- (SR %*% vt)[episode[experience]]
       tridx <- tridx+1
     }
   }
   return(SR.EV)
+}
+
+seq.Lynn.EV <- function(tr, v, goal, stepsleft, betap, winM, loseM){
+  library(matrixcalc)
+  Lynn.EV <- rep(0,length(v))   # Initialize eventual predictions of EV
+  tridx   <- 1                  # Initialize indexing variable to fill SR.EV
+  ntilde  <- matrix(0,20,20)    # Initialize tally of transitions
+  Ahat    <- matrix(0,20,20)    # Initialize empty estimated transition matrix
+  
+  for(tri in 1:max(tr)){
+    vcur <- v[tr==tri]    # Select current episode of node experiences
+    ## Perform EV estimate for first node
+    # Get initial state vector
+    I <- rep(0,20); I[vcur[1]] <- 1
+    # Transition matrix with goal state as absorbing
+    Ahat.goal <- Ahat
+    Ahat.goal[goal[tridx],] = 0 
+    hitVec <- rep(0,15) #vector of hitting chances
+    for(s in stepsleft[tridx]:1){#maybe :1?
+      hitC <- I %*% matrix.power(Ahat.goal, s)
+      hitVec[s] <- hitC[goal[tridx]]
+    }
+    Lynn.EV[tridx] <- sum(hitVec)*winM + (1-sum(hitVec)) * loseM
+    tridx <- tridx+1
+    
+    for(ep in 2:length(vcur)){
+      ## Update ntilde and Ahat
+      Pdt <- sapply((ep-1):1, function(dt){exp(-betap*dt) / sum(sapply((ep-1):1, function(sdt){exp(-betap*sdt)}))})
+      for(epdt in 1:(ep-1)){
+        ntilde[vcur[epdt],vcur[ep]] <- ntilde[vcur[epdt],vcur[ep]] + Pdt[epdt]
+        Ahat[vcur[epdt],] <- ntilde[vcur[epdt],] / sum(ntilde[vcur[epdt],])
+      }
+      
+      ## Get Lynn EV
+      # Get initial state vector
+      I <- rep(0,20); I[vcur[ep]] <- 1
+      # Transition matrix with goal state as absorbing
+      Ahat.goal <- Ahat
+      Ahat.goal[goal[tridx],] = 0 
+      hitVec <- rep(0,15) #vector of hitting chances
+      for(s in stepsleft[tridx]:1){#maybe :1?
+        hitC <- I %*% matrix.power(Ahat.goal, s)
+        hitVec[s] <- hitC[goal[tridx]]
+      }
+      Lynn.EV[tridx] <- sum(hitVec)*winM + (1-sum(hitVec)) * loseM
+      if(Lynn.EV[tridx] > 5){browser()}
+      tridx <- tridx+1
+    }
+  }
+  return(list(Ahat=Ahat, Lynn.EV=Lynn.EV))
+}
+
+get.opt.choice <- function(tr, v, goal, stepsleft, tMat, winM, loseM){
+  opt.EV <- rep(0, length(v))
+  for(toti in 1:length(v)){
+    I <- rep(0,20); I[v[toti]]<-1
+    if(stepsleft[toti]==0|v[toti]==goal[toti]){
+      if(v[toti]==goal[toti]){opt.EV[toti]<-winM}else{opt.EV[toti]<-loseM}
+    }else{
+      hitVec <- rep(0,15)
+      tMat.goal <- tMat
+      tMat.goal[goal[toti],] <- 0
+      for(s in stepsleft[toti]:1){
+        hitC <- I %*% matrix.power(tMat.goal, s)
+        hitVec[s] <- hitC[goal[toti]]
+      }
+      opt.EV[toti] <- sum(hitVec)*winM + (1-sum(hitVec)) * loseM
+    }
+  }
+  opt.choice <- as.numeric(opt.EV>0)
+  return(opt.choice)
 }
--- Rsims/tests/QuadMod_bet_SREV.R
+++ Rsims/tests/QuadMod_bet_SREV.R
@@ -7,7 +7,7 @@ sapply(paste0('src/',list.files('src/')), source)
 winM   <- 5
 nVisit <- 15
 nTr    <- 100
-nPP    <- 500
+nPP    <- 40
 
 # Play around with SR learning
 lr <- 0.05
@@ -24,7 +24,6 @@ for(trial in 1:nTr){
   }
 }
 heatmap(SR, Rowv = NA, Colv = NA)
-heatmap(eigen(SR)$vectors[,1:7], Rowv=NA, Colv=NA)
 
 # Make choice predictions?
 ct <- 0.60
@@ -32,17 +31,128 @@ vt <- rep(-(ct^3), 20); vt[8] <- winM
 SR %*% vt
 
 # Plot SREV over time
-SR.EVreg <- seq.SR.EV(full.exp[pp==1,tr], full.exp[pp==1,v], full.exp[pp==1,goal], full.exp[pp==1,stepsleft], lr, gamm, ct, winM)
+SR.EVreg <- seq.SR.EV(full.exp[pp==1,tr], full.exp[pp==1,v], full.exp[pp==1,goal], full.exp[pp==1,stepsleft], lr, gamm, ct, winM=3)
 plot(SR.EVreg,
      type='l')
 abline(0, b=0, col='red')
 
 # Get SREV based choices! 
 choiceS <- sapply(boot::inv.logit(2*SR.EVreg), function(b){rbinom(1,1,b)})
-plot.dat <- data.table(sym.id = full.exp[pp==1,sym.id], pol.type = full.exp[pp==1, if(trtype=='deep'){'deep'}else{'bottleneck'}, by=.(tr,stepsleft)]$V1,
-                       stepsleft = full.exp[pp==1,stepsleft], choiceS=choiceS)
+plot.dat <- data.table(sym.id = full.exp[pp==1,sym.id], pol.type = full.exp[pp==1,pol.type], stepsleft=full.exp[pp==1,stepsleft],choiceS=choiceS)
 
 ggplot(plot.dat[,sum(choiceS==1)/.N, by=.(sym.id,pol.type,stepsleft)]) +
   geom_line(aes(x=stepsleft, y=V1)) +
   geom_point(aes(x=stepsleft, y=V1)) +
   facet_grid(pol.type~sym.id)
+
+#### LYNN EV tryouts ----
+Lynn.EVreg <- seq.Lynn.EV(full.exp[pp==1,tr], full.exp[pp==1,v], full.exp[pp==1,goal], full.exp[pp==1,stepsleft], betap=0.3, winM=3, loseM=-1)
+
+heatmap(Lynn.EVreg$Ahat, Rowv = NA, Colv = NA)
+
+plot(Lynn.EVreg$Lynn.EV, type='l')
+abline(0, b=0, col='red')
+
+choiceS <- sapply(boot::inv.logit(2*Lynn.EVreg$Lynn.EV), function(b){rbinom(1,1,b)})
+plot.dat <- data.table(sym.id = full.exp[pp==1,sym.id], pol.type = full.exp[pp==1, pol.type], stepsleft = full.exp[pp==1,stepsleft], choiceS=choiceS)
+ggplot(plot.dat[,sum(choiceS==1)/.N, by=.(sym.id,pol.type,stepsleft)]) +
+  geom_line(aes(x=stepsleft, y=V1)) +
+  geom_point(aes(x=stepsleft, y=V1)) +
+  facet_grid(pol.type~sym.id)
+
+# Get choices into data.table
+lc.exp <- full.exp[pp==1,list(pp,tr,stepsleft,v,goal,choiceS=choiceS,result)
+                   ][choiceS==0,choiceS:=-1
+                   ][,betS:=bet.sum(choiceS,5),by=.(pp,tr)
+                   ][stepsleft==0 & v!=goal, eRew:=-betS
+                   ][v==goal, eRew:=betS*3
+                   ][,opt.choice:=get.opt.choice(tr,v,goal,stepsleft,tMat,winM=3,loseM=-1)
+                   ][opt.choice==0,opt.choice:=-1
+                   ][,opt.bet:=bet.sum(opt.choice,5),by=.(pp,tr)
+                   ][stepsleft==0 & v!=goal, opt.Rew:=-opt.bet
+                   ][v==goal, opt.Rew:=opt.bet*3]
+
+lc.exp[,bet.sum(na.omit(eRew),0)]
+
+# Plot accumulation of reward throughout experiment: Lynnlearn v optEV
+plot(lc.exp[!is.na(opt.Rew),bet.sum(na.omit(opt.Rew),0)],type='l', col='green')
+lines(lc.exp[!is.na(eRew),bet.sum(na.omit(eRew),0)], type='l', col='purple')
+
+# Reward rate Lynnlearn / optEV
+ggplot(lc.exp[!is.na(opt.Rew),list(tr, rwrat=eRew-opt.Rew)])+
+  geom_line(aes(x=tr,y=rwrat))
+
+# Proportion of optimal choices (choiceS==opt.choice/.N)
+ggplot(lc.exp[,sum(choiceS==opt.choice)/.N, by=tr]) +
+  geom_line(aes(x=tr,y=V1))
+
+# Per 4 trials, proportion of optimal choices
+lc.exp[,list(group=sum((tr/1:25)>4)+1, opt.choice,choiceS),by=.(pp,tr,stepsleft)
+       ][,sum(opt.choice==choiceS)/.N, by=group
+       ][,ggplot(.SD, aes(x=group,y=V1))+
+           geom_line()]
+
+## Get LynnEV and choices per participant - Compare with 
+lc.exp <- full.exp[,list(pp,tr,stepsleft,v,goal)
+                   ][,LynnEV:=seq.Lynn.EV(tr,v,goal,stepsleft,betap=100,winM=2,loseM=-1)$Lynn.EV,by=pp
+                   ][stepsleft==0 & v!=goal, LynnEV:=-1
+                   ][v==goal, LynnEV:=2]
+lc.exp[,LynnC:=rbinom(1,1,boot::inv.logit(Inf*LynnEV)),by=.(pp,tr,stepsleft)
+       ][LynnC==0,LynnC:=-1
+       ][,OptC:=get.opt.choice(tr,v,goal,stepsleft,tMat,winM=2,loseM=-1),by=pp
+       ][OptC==0,OptC:=-1
+       ][,c('LynnBet','OptBet'):=list(bet.sum(LynnC,5), bet.sum(OptC,5)), by=.(pp,tr)
+       ][stepsleft==0 & v!=goal, c('LynnRew','OptRew'):=list(-LynnBet, -OptBet)
+       ][v==goal, c('LynnRew','OptRew'):=list(LynnBet*2, OptBet*2)]
+
+# Plot of proportion of optimal choices by trial
+lc.exp[,list(group=sum((tr/1:25)>4)+1, LynnC, OptC),by=.(pp,tr,stepsleft)
+       ][,pp:=as.factor(pp)
+       ][,list(propC=sum(LynnC==OptC)/.N), by=.(pp,tr)
+       ][,ggplot(.SD)+
+           geom_line(aes(group=pp, x=tr, y=propC, col=pp))+
+           geom_line(data=.SD[,list(meanProp=mean(propC)),by=tr],aes(x=tr,y=meanProp))]
+# Plot of proportion of optimal choices by group
+lc.exp[,list(group=sum((tr/1:25)>4)+1, LynnC, OptC),by=.(pp,tr,stepsleft)
+       ][,pp:=as.factor(pp)
+       ][,list(propC=sum(LynnC==OptC)/.N), by=.(pp,group)
+       ][,ggplot(.SD)+
+           geom_line(aes(group=pp, x=group,y=propC,col=pp),alpha=0.2)+
+           geom_line(data=.SD[,list(meanProp=mean(propC)),by=group], aes(x=group,y=meanProp))]
+# Plot of reward rate over group
+lc.exp[!is.na(LynnRew),list(group=sum((tr/1:25)>4)+1, LynnRew, OptRew),by=.(pp,tr,stepsleft)
+       ][,pp:=as.factor(pp)
+       ][,list(grRew=sum(LynnRew), grOpt=sum(OptRew)),by=.(pp,group)
+       ][,ggplot(.SD)+
+           geom_line(aes(group=pp, x=group, y=grRew),col='purple',alpha=0.2)+
+           geom_line(aes(group=pp, x=group, y=grOpt),col='green',alpha=0.2)+
+           geom_line(data=.SD[,list(m=mean(grRew)),by=group], aes(x=group, y=m), col='purple')+
+           geom_line(data=.SD[,list(m=mean(grOpt)),by=group], aes(x=group, y=m), col='green')]
+
+## Get SREV and choices
+sr.exp <- full.exp[,list(pp,tr,stepsleft,v,goal)
+                   ][,SREV:=seq.SR.EV(tr,v,goal,stepsleft,lr=0.05,gamm=0.95,ct=0.6,winM=1),by=pp
+                   ][,SRC:=rbinom(1,1,boot::inv.logit(Inf*SREV)),by=.(pp,tr,stepsleft)
+                   ][SRC==0, SRC:=-1
+                   ][,OptC:=get.opt.choice(tr,v,goal,stepsleft,tMat,winM=1,loseM=-1),by=pp
+                   ][OptC==0, OptC:=-1
+                   ][,c('SRBet','OptBet'):=list(bet.sum(SRC,5), bet.sum(OptC,5)), by=.(pp,tr)
+                   ][stepsleft==0 & v!=goal, c('SRRew','OptRew'):=list(-SRBet, -OptBet)
+                   ][v==goal, c('SRRew','OptRew'):=list(SRBet, OptBet)]
+
+sr.exp[,list(group=sum((tr/1:25)>4)+1,SRC,OptC),by=.(pp,tr,stepsleft)
+       ][,pp:=as.factor(pp)
+       ][,list(propC=sum(SRC==OptC)/.N), by=.(pp,tr)
+       ][,ggplot(.SD)+
+           geom_line(aes(group=pp,col=pp,x=tr,y=propC))+
+           geom_line(data=.SD[,list(m=mean(propC)),by=tr],aes(x=tr,y=m))]
+
+sr.exp[!is.na(SRRew),list(group=sum((tr/1:25)>4)+1,SRRew,OptRew),by=.(pp,tr,stepsleft)
+       ][,pp:=as.factor(pp)
+       ][,list(grRew=sum(SRRew), grOpt=sum(OptRew)),by=.(pp,group)
+       ][,ggplot(.SD)+
+           geom_line(aes(group=pp,x=group,y=grRew),col='purple',alpha=0.1)+
+           geom_line(data=.SD[,list(m=mean(grRew)),by=group],aes(x=group,y=m),col='purple')+
+           geom_line(aes(x=group,y=grOpt,group=pp),col='green',alpha=0.1)+
+           geom_line(data=.SD[,list(m=mean(grOpt)),by=group],aes(x=group,y=m),col='green')]
+
