37cbeb55ecbd39efe29cbb50b99cac9c5f38d229
Sven <wientjes.s@gmail.com>
2021-04-20 12:08:24 +0200

    Update HBI models and fixation images

--- Online_Experiment/js/FixationCanvas.js
+++ Online_Experiment/js/FixationCanvas.js
@@ -1,11 +1,5 @@
 function FixationC(miss){
     var canvas = document.getElementById('FixationCv');
-    //canvas.width = window.innerWidth;
-    //canvas.height = window.innerHeight;
-    canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight;
-    canvasW = canvas.width;
-    canvasH = canvas.height;
     ctx = canvas.getContext("2d");
 
     // Text
@@ -17,32 +11,7 @@ function FixationC(miss){
     }
     ctx.textAlign = 'center';
     ctx.fillText('+', canvas.width/2, canvas.height/2);
-
-    var img = new Image();
-    img.src="img/Hallway.png"
-    img.onload = function(e){
-        ctx.drawImage(img,0,0,canvas.width, canvas.height)
-        ctx.fillText('+', canvas.width/2, canvas.height/2);
-    }
 }
 
-/*function testFix(c){
-    c.width = document.body.clientWidth;
-    c.height = document.body.clientHeight;
-    var ctx=c.getContext('2d');
-    ctx.font = '70px Arial';
-    ctx.fillStyle = 'black';
-    ctx.textAlign = 'center';
-    ctx.fillText('+', c.width/2, c.height/2);
-
-    var img = new Image();
-    img.src="img/Hallway.png"
-    img.onload = function(e){
-        ctx.drawImage(img,0,0,c.width, c.height)
-        ctx.fillText('+', c.width/2, c.height/2);
-    }
-}
-
-function testDraw(){
-
-}*/
+var FixStr = '<div id="FixationDiv" style="margin:0 auto;">'+
+'<canvas id="FixationCv" width="1920" height="1080" style="background:url(img/Hallway.png)"></canvas></div>';
---
+++ Online_Experiment/js/StimulusCanvas.js
@@ -0,0 +1,34 @@
+function StimC(trBet){
+    var canvas = document.getElementById('StimulusCv');
+    cst = canvas.getContext("2d");
+
+    // Draw circles
+    
+    cst.fillStyle = 'rgba(221, 204, 8, 1)'
+    cst.strokeStyle = 'black'
+    circStart = 580;
+    for(i=0; i<trBet; i++){
+        cst.beginPath();
+        cst.arc(circStart,1060,20,0,2*Math.PI);
+        cst.fill();
+        cst.stroke();
+        circStart += 40;
+    }
+    window.addEventListener('keydown', BetLive);
+
+}
+
+function BetLive(e){
+    //react to a buttonpress
+    var keyCode = e.key;
+    if(keyCode == 'm' || keyCode == 'M'){
+        cst.beginPath();
+        cst.arc(circStart,1060,20,0,2*Math.PI);
+        cst.fill();
+        cst.stroke();
+        window.removeEventListener('keydown', BetLive);
+    }else if(keyCode == 'z' || keyCode == 'Z'){
+        cst.clearRect(circStart-60,1040,40,40);
+        window.removeEventListener('keydown', BetLive);
+    }
+}
