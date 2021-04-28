92d5f3562af7ae75f7ee16871fb4915bfd563804
Sven <wientjes.s@gmail.com>
2021-04-28 16:47:46 +0200

    Telelist generate and save for 40pp

--- Rsims/tests/Schapiro_bet.R
+++ Rsims/tests/Schapiro_bet.R
@@ -656,51 +656,76 @@ close(write1sub)
 
 ################################################################################
 # Get pairs for bottleneck-hallway teleporter question #
-probeQs <- list(a = c(15,1), b = c(5,6), c = c(10,11))
-allMat <- matrix(0,nrow=1,ncol=2)
-# All deep-deep
-for(i in 1:3){
-  curMat <- matrix(nrow=1, ncol=2)
-  curMat[1,] <- probeQs[[i]]
-  # Pick deep-deep transition per cluster
-  for(j in 1:2){
-    # Which cluster we draw from
-    cur.c <- names(idmap.bt)[sapply(idmap.bt, function(nd){curMat[1,j] %in% nd})]
-    # Choose a deep node
-    one.d <- sample(idmap.d[[cur.c]],1)
-    # Check which rows already have this node
-    #which(allMat[,1]==one.d | allMat[,2]==one.d)
-    # Sample the next node
-    two.d <- sample(idmap.d[[cur.c]][which(idmap.d[[cur.c]]!=one.d)],1)
-    # If the pair already exists, sample the other one
-    if(any(allMat[,1]==one.d & allMat[,2]==two.d | allMat[,1]==two.d & allMat[,2]==one.d)){
-      two.d <- idmap.d[[cur.c]][which(idmap.d[[cur.c]]!=one.d & idmap.d[[cur.c]]!=two.d)]
+allProbeQs <- list()
+for(p in 1:nPP){
+  probeQs <- list(a = c(15,1), b = c(5,6), c = c(10,11))
+  allMat <- matrix(0,nrow=1,ncol=2)
+  # All deep-deep
+  for(i in 1:3){
+    curMat <- matrix(nrow=1, ncol=2)
+    curMat[1,] <- probeQs[[i]]
+    # Pick deep-deep transition per cluster
+    for(j in 1:2){
+      # Which cluster we draw from
+      cur.c <- names(idmap.bt)[sapply(idmap.bt, function(nd){curMat[1,j] %in% nd})]
+      # Choose a deep node
+      one.d <- sample(idmap.d[[cur.c]],1)
+      # Check which rows already have this node
+      #which(allMat[,1]==one.d | allMat[,2]==one.d)
+      # Sample the next node
+      two.d <- sample(idmap.d[[cur.c]][which(idmap.d[[cur.c]]!=one.d)],1)
+      # If the pair already exists, sample the other one
+      if(any(allMat[,1]==one.d & allMat[,2]==two.d | allMat[,1]==two.d & allMat[,2]==one.d)){
+        two.d <- idmap.d[[cur.c]][which(idmap.d[[cur.c]]!=one.d & idmap.d[[cur.c]]!=two.d)]
+      }
+      # Add samples to current and overall counters
+      allMat <- rbind(allMat, c(one.d, two.d))
+      curMat <- rbind(curMat, c(one.d, two.d))
     }
-    # Add samples to current and overall counters
-    allMat <- rbind(allMat, c(one.d, two.d))
-    curMat <- rbind(curMat, c(one.d, two.d))
+    probeQs[[i]] <- curMat
   }
-  probeQs[[i]] <- curMat
-}
-# All bottleneck-deep
-for(i in 1:3){
-  curMat <- probeQs[[i]]
-  for(j in 1:2){
-    # Which cluster we draw from
-    cur.c <- names(idmap.bt)[sapply(idmap.bt, function(nd){curMat[1,j] %in% nd})]
-    # Get the bottleneck node not part of the cluster transition
-    fakeBn <- idmap.bt[[cur.c]][which(idmap.bt[[cur.c]] != curMat[1,j])]
-    # Connect with deep node (not yet sampled twice)
-    el.deeps <- idmap.d[[cur.c]][which(sapply(idmap.d[[cur.c]], function(dn){sum(allMat==dn)}) == 1)]
-    el.deeps <- el.deeps[!sapply(el.deeps, function(ed){ed %in% curMat})]
-    conDeep  <- el.deeps[sample(1:length(el.deeps),1)]
-    # Add samples to current and overall counters
-    allMat <- rbind(allMat, c(fakeBn, conDeep))
-    curMat <- rbind(curMat, c(fakeBn, conDeep))
+  # All bottleneck-deep
+  for(i in 1:3){
+    curMat <- probeQs[[i]]
+    for(j in 1:2){
+      # Which cluster we draw from
+      cur.c <- names(idmap.bt)[sapply(idmap.bt, function(nd){curMat[1,j] %in% nd})]
+      # Get the bottleneck node not part of the cluster transition
+      fakeBn <- idmap.bt[[cur.c]][which(idmap.bt[[cur.c]] != curMat[1,j])]
+      # Connect with deep node (not yet sampled twice)
+      el.deeps <- idmap.d[[cur.c]][which(sapply(idmap.d[[cur.c]], function(dn){sum(allMat==dn)}) == 1)]
+      el.deeps <- el.deeps[!sapply(el.deeps, function(ed){ed %in% curMat})]
+      conDeep  <- el.deeps[sample(1:length(el.deeps),1)]
+      # Add samples to current and overall counters
+      allMat <- rbind(allMat, c(fakeBn, conDeep))
+      curMat <- rbind(curMat, c(fakeBn, conDeep))
+    }
+    probeQs[[i]] <- rbind(probeQs[[i]], curMat[-c(1,2,3),])
   }
-  probeQs[[i]] <- rbind(probeQs[[i]], curMat[-c(1,2,3),])
+  for(trI in 1:3){
+    probeQs[[trI]] <- probeQs[[trI]][c(sample(1:5)),] # Shuffle rows (question order)
+  }
+  probeQsShuf <- list()
+  qTypes <- c('a','b','c')
+  for(trI in 1:3){
+    curType <- sample(1:length(qTypes),1)
+    probeQsShuf[[trI]] <- probeQs[[qTypes[curType]]]
+    qTypes <- qTypes[-curType]
+  }
+  allProbeQs[[p]] <- probeQsShuf
 }
 
+# Write to js
+teleWrite <- paste0('var telelist = [',
+  paste('[',sapply(1:nPP, function(Pns){
+    paste('[',sapply(1:3, function(comb){
+      paste('[',sapply(1:5, function(opt){
+        paste(allProbeQs[[Pns]][[comb]][opt,], collapse=', ')
+      }),']', sep='', collapse=', ')
+    }),']', collapse=', ',sep='')
+  }), ']', collapse=', ',sep=''),
+'];')
 
-
-
+write1sub <- file('data/telelist.js')
+writeLines(teleWrite, write1sub)
+close(write1sub)
