26f95705938947f35e95d9c713e2b2dad73d09ca
Sven <wientjes.s@gmail.com>
2021-04-28 16:49:17 +0200

    Partition js screens and add Teleport Question
    vectors for stimulus and goal images are now in a separate js file from the feedback screens

--- Online_Experiment/js/FeedbackScreens.js
+++ Online_Experiment/js/FeedbackScreens.js
@@ -1,26 +1,14 @@
-// List of room images (idx = node nr)
-var roomsdef = ['img/Balloon.png', 'img/Basket.png', 'img/Book.png', 'img/Boot.png', 'img/Cap.png', 
-'img/Clipboard.png', 'img/Floppy.png', 'img/Fryingpan.png', 'img/Jar.png',
-'img/Lightbulb.png', 'img/Mug.png', 'img/Ovenmitt.png', 'img/Soapdispenser.png',
-'img/Toaster.png', 'img/Toiletpaper.png'];
-
-// List of goal images (idx = node nr -> correspond to rooms!)
-var goalsdef = ['img/Balloon_goal.png', 'img/Basket_goal.png', 'img/Book_goal.png', 'img/Boot_goal.png', 'img/Cap_goal.png', 
-'img/Clipboard_goal.png', 'img/Floppy_goal.png', 'img/Fryingpan_goal.png', 'img/Jar_goal.png',
-'img/Lightbulb_goal.png', 'img/Mug_goal.png', 'img/Ovenmitt_goal.png', 'img/Soapdispenser_goal.png',
-'img/Toaster_goal.png', 'img/Toiletpaper_goal.png'];
-
 // Defines the strings to pass through blockend.
 function miniblockFb(mbend){
-    if(mbend=='downgoal'||mbend=='upgoal'){
+    if(mbend=='downgoal'||mbend=='upgoal'||mbend=='missgoal'){
         return '<p>Congratulations! You have reached the goal.</p>'+
         '<p>You have been awarded the corresponding points</p>'
-    }else if(mbend=='downend'||mbend=='upend'){
+    }else if(mbend=='downend'||mbend=='upend'||mbend=='missend'){
         return '<p>There was no goal in this miniblock.</p>'+
         '<p>Better luck next time! Maybe you could have prevented some losses.</p>'
     }
 }
-
+// Full feedback screen (after blockend message, before new miniblock)
 function fbScreen(nSteps, nGoals, trRew, trLeft, total_reward, mbPunish){
     if(trRew >= 0 & total_reward >= 0 & mbPunish == 0){
         return '<p>In this block you made <span style="color: #93c54b">' +
---
+++ Online_Experiment/js/stimLists.js
@@ -0,0 +1,11 @@
+// List of room images (idx = node nr)
+var roomsdef = ['img/Balloon.png', 'img/Basket.png', 'img/Book.png', 'img/Boot.png', 'img/Cap.png', 
+'img/Clipboard.png', 'img/Floppy.png', 'img/Fryingpan.png', 'img/Jar.png',
+'img/Lightbulb.png', 'img/Mug.png', 'img/Ovenmitt.png', 'img/Soapdispenser.png',
+'img/Toaster.png', 'img/Toiletpaper.png'];
+
+// List of goal images (idx = node nr -> correspond to rooms!)
+var goalsdef = ['img/Balloon_goal.png', 'img/Basket_goal.png', 'img/Book_goal.png', 'img/Boot_goal.png', 'img/Cap_goal.png', 
+'img/Clipboard_goal.png', 'img/Floppy_goal.png', 'img/Fryingpan_goal.png', 'img/Jar_goal.png',
+'img/Lightbulb_goal.png', 'img/Mug_goal.png', 'img/Ovenmitt_goal.png', 'img/Soapdispenser_goal.png',
+'img/Toaster_goal.png', 'img/Toiletpaper_goal.png'];
