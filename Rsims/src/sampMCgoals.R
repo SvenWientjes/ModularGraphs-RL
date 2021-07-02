8a3e9b70a4fe0827f3ed63111270359f6a632a52
Sven <wientjes.s@gmail.com>
2021-07-02 16:23:04 +0200

    New testing phase

--- Rsims/src/sampMCgoals.R
+++ Rsims/src/sampMCgoals.R
@@ -73,6 +73,19 @@ trajSamp.max <- function(start.v, goal.v, Edges, maxL){
   }
 }
 
+pad.random.walk <- function(start.v, end.v, goal.v, Edges, minL, maxL){
+  # Start RW
+  v <- start.v
+  # Get true RW up until min length
+  while(length(v)<minL | goal.v %in% v | !v[length(v)]==end.v){
+    v <- start.v
+    while((length(v)<minL | !v[length(v)]==end.v) & !length(v)>maxL){
+      v[length(v)+1] <- sample(Edges[[v[length(v)]]],1)
+    }
+  }
+  return(v)
+}
+
 gen.exhaust.experiment <- function(nTrs, goalytics, idmap.d, idmap.dg2, idmap.bg5, idmap.bg6, gsym5, gsym6, Edges, nVisit, nPP, HamCyc=0){
   all.telomeres <- data.table(pp=0, s.type='init', g.type='init', clust.loc='init', v=0, goal=0, sym.id='init')
   all.exp <- data.table(pp=0, miniblock=0, v=0, nSteps=0, goal=0)
@@ -154,6 +167,181 @@ exp.to.js <- function(exp.list, nTrs, nPP){
   write1sub <- file('data/trajectories.js')
   writeLines(test2sub, write1sub)
   close(write1sub)
+}
+
+gen.train <- function(nTrs, goalytics, idmap.d, idmap.dg2, idmap.bg5, idmap.bg6, gsym5, gsym6, Edges, nVisit, nPP){
+  all.telomeres <- data.table(pp=0, s.type='init', g.type='init', clust.loc='init', v=0, goal=0, sym.id='init')
+  all.exp <- data.table(pp=0, miniblock=0, v=0, nSteps=0, goal=0)
+  for(ppn in 1:nPP){
+    telomeres <- sampMCgoals(nTrs, goalytics, idmap.d, idmap.dg2, idmap.bg5, idmap.bg6, gsym5, gsym6)
+    # Rework into sequence of 'camps' (intermediate goal/starts)
+    camps <- data.table(v.type = as.character(shift(telomeres$g.type, 1, type='lag')), v = telomeres$v)
+    camps <- rbind(camps, data.table(v.type=as.character(telomeres[.N,g.type]), v=telomeres[.N,goal]))
+    camps$v.type[1] <- 'start'
+    camps$miniblock <- 0:nTrs
+    
+    # Monte Carlo sampling of trajectories with t.reach requirements
+    gen.exp <- data.table(miniblock=0, v=0, nSteps=0, goal=0)
+    for(cmp in 1:nTrs){
+      goal.v <- camps[miniblock==cmp,v]
+      start.v <- camps[miniblock==(cmp-1),v]
+      v <- trajSamp.max(start.v,goal.v,Edges,nVisit)
+      print(cmp)
+      gen.exp <- rbind(gen.exp, data.table(miniblock=cmp, v=v, nSteps=((length(v)):1)-1, goal=goal.v))
+    }
+    gen.exp <- gen.exp[-1,]
+    all.exp <- rbind(all.exp, gen.exp[,pp:=ppn])
+    all.telomeres <- rbind(all.telomeres,telomeres[,pp:=ppn])
+  }
+  return(list(all.exp[-1,], all.telomeres[-1,]))
+}
+
+gen.D.Ham <- function(start.v, goal.v, minRW, maxRW, idmap.d, idmap.dg2, c.map, idmap.bt, Edges){
+  #minRW/maxRW: Minimum / maximum length of initial padded random walk
+  #v.start    : True starting identity (start of RW)
+  #v.goal     : True identity of goal node
+  ##################################################################
+  # Get valid symmetry for goal
+  rotval <- (which(sapply(idmap.d, function(li){goal.v %in% li}))-1) * 5
+  # Get valid start for Hamiltonian portion
+  ham.s <- sample((((idmap.dg2[['b']]+rotval)-1)%%15)+1, 1)
+  # Get symmetries (start, goal, lateral)
+  ham.s.sym <- names(idmap.d)[which(sapply(idmap.d, function(li){ham.s %in% li}))]
+  g.sym <- names(idmap.d)[which(sapply(idmap.d, function(li){goal.v %in% li}))]
+  l.sym <- c('a','b','c')[which(!c('a','b','c')%in%c(ham.s.sym,g.sym))]
+  # Get initial random walk (last value of RW is already b1)
+  v <- pad.random.walk(start.v, ham.s, goal.v, Edges, minRW+1, maxRW+1)
+  
+  ## Get relevant test walk
+  # Same cluster as ham.s
+  el.b <- idmap.d[[ham.s.sym]]
+  v <- c(v, sample(el.b[which(el.b != ham.s)]))
+  # Between transition
+  v <- c(v, idmap.bt[[ham.s.sym]][which(c.map[[ham.s.sym]]==l.sym)])
+  v <- c(v, idmap.bt[[l.sym]][which(c.map[[l.sym]]==ham.s.sym)])
+  # Deep nodes of lateral cluster
+  v <- c(v, sample(idmap.d[[l.sym]]))
+  # Into the goal cluster
+  v <- c(v, idmap.bt[[l.sym]][which(c.map[[l.sym]]==g.sym)])
+  v <- c(v, idmap.bt[[g.sym]][which(c.map[[g.sym]]==l.sym)])
+  # Two non-goal deep goal nodes
+  v <- c(v, sample(idmap.d[[g.sym]][which(idmap.d[[g.sym]]!=goal.v)]))
+  v <- c(v, idmap.bt[[g.sym]][which(c.map[[g.sym]]==ham.s.sym)])
+  v <- c(v, goal.v)
+  return(v)
+}
+
+gen.D.Min <- function(start.v, goal.v, minRW, maxRW, idmap.d, idmap.dg2, c.map, idmap.bt, Edges){
+  #minRW/maxRW: Minimum / maximum length of initial padded random walk
+  #v.start    : True starting identity (start of RW)
+  #v.goal     : True identity of goal node
+  ##################################################################
+  # Get valid symmetry for goal
+  rotval <- (which(sapply(idmap.d, function(li){goal.v %in% li}))-1) * 5
+  # Get valid start for Hamiltonian portion
+  ham.s <- sample((((idmap.dg2[['c']]+rotval)-1)%%15)+1, 1)
+  # Get symmetries (start, goal, lateral)
+  ham.s.sym <- names(idmap.d)[which(sapply(idmap.bt, function(li){ham.s %in% li}))]
+  g.sym <- names(idmap.d)[which(sapply(idmap.d, function(li){goal.v %in% li}))]
+  l.sym <- c('a','b','c')[which(!c('a','b','c')%in%c(ham.s.sym,g.sym))]
+  # Get initial random walk
+  v <- pad.random.walk(start.v, ham.s, goal.v, Edges, minRW, maxRW)
+  # Quick move through goal cluster
+  v <- c(v, idmap.bt[[g.sym]][which(c.map[[g.sym]]==ham.s.sym)])
+  v <- c(v, sample(idmap.d[[g.sym]][which(idmap.d[[g.sym]]!=goal.v)],1))
+  v <- c(v, idmap.bt[[g.sym]][which(c.map[[g.sym]]==l.sym)])
+  # Move into lateral sym and get all nodes
+  v <- c(v, idmap.bt[[l.sym]][which(c.map[[l.sym]]==g.sym)])
+  v <- c(v, sample(idmap.d[[l.sym]]))
+  # b -> a transition and successive a -> a transition!
+  v <- c(v, idmap.bt[[l.sym]][which(c.map[[l.sym]]==ham.s.sym)])
+  v <- c(v, idmap.bt[[ham.s.sym]][which(c.map[[ham.s.sym]]==l.sym)])
+  # Sample one node from s sym and move into goal cluster
+  v <- c(v, sample(idmap.d[[ham.s.sym]],1))
+  v <- c(v, idmap.bt[[ham.s.sym]][which(c.map[[ham.s.sym]]==g.sym)])
+  v <- c(v, idmap.bt[[g.sym]][which(c.map[[g.sym]]==ham.s.sym)])
+  # Sample two deep goal nodes
+  v <- c(v, sample(idmap.d[[g.sym]][which(idmap.d[[g.sym]]!=goal.v)]))
+  v <- c(v, idmap.bt[[g.sym]][which(c.map[[g.sym]]==l.sym)])
+  v <- c(v, goal.v)
+  return(v)
+}
+
+gen.B.Min <- function(start.v, goal.v, minRW, maxRW, idmap.d, idmap.bg5,idmap.bg6, gsym5, gsym6, c.map, idmap.bt, Edges){
+  if(goal.v %in% gsym5){ # Rotational symmetry w.r.t. 5 start
+    val.sym <- idmap.bg5
+    rotval <- (which(gsym5==goal.v)-1)*5
+  }else if(goal.v %in% gsym6){ # Rotational symmetry w.r.t. 6 start
+    val.sym <- idmap.bg6
+    rotval <- (which(gsym6==goal.v)-1)*5
+  }
+  # Get valid start for Hamiltonian portion
+  ham.s <- sample((((val.sym[['g']]+rotval)-1)%%15)+1, 1)
+  val.sym <- sapply(val.sym, function(li){((li+rotval-1)%%15)+1})
+  # Get symmetries (start, goal, lateral)
+  ham.s.sym <- names(idmap.d)[which(sapply(idmap.bt, function(li){ham.s %in% li}))]
+  g.sym <- names(idmap.d)[which(sapply(idmap.bt, function(li){goal.v %in% li}))]
+  l.sym <- c('a','b','c')[which(!c('a','b','c')%in%c(ham.s.sym,g.sym))]
+  # Get initial random walk (last value of RW is already b1)
+  v <- pad.random.walk(start.v, ham.s, goal.v, Edges, minRW+1, maxRW+1)
+  # Move to the cluster with c and capture c
+  v <- c(v, val.sym[['h']], val.sym[['a']])
+  bs <- sample(val.sym[['b']])
+  v <- c(v, bs[c(1,2)], val.sym[['c']], bs[3], val.sym[['a']], val.sym[['h']])
+  # Move through starting cluster, avoiding starting deep node
+  v <- c(v, sample(val.sym[['g']][which(val.sym[['g']]!=ham.s)]), val.sym[['f']])
+  v <- c(v, val.sym[['e']], sample(val.sym[['d']]), goal.v)
+  return(v)
+}
+
+gen.exp <- function(nDh, nDm, nBm, minRW, maxRW, nTrs, nVisit, nPP, goalytics, idmap.d, idmap.bt, c.map, idmap.dg2, idmap.bg5, idmap.bg6, gsym5, gsym6, Edges){
+  full.exp <- data.table(pp=0, miniblock=0, v=0, nSteps=0, goal=0)
+  all.telomeres <- data.table(pp=0, s.type='init',g.type='init',clust.loc='init',v=0,goal=0,sym.id='init')
+  for(ppn in 1:nPP){
+    get.exp <- gen.train(nTrs, goalytics, idmap.d, idmap.dg2, idmap.bg5, idmap.bg6, gsym5, gsym6, Edges, nVisit, 1)
+    get.exp[[1]][,pp:=ppn]
+    get.exp[[2]][,pp:=ppn]
+    full.exp <- rbind(full.exp, get.exp[[1]])
+    all.telomeres <- rbind(all.telomeres, get.exp[[2]])
+    # Tally of test trials
+    test.tally <- c(nDh, nDm, nBm)
+    while(sum(test.tally)>0){
+      # Sample type of test trial
+      cur.T <- which(test.tally>0)[sample(length(which(test.tally>0)),1)]
+      # Check which node we have ended in
+      all.n <- list('a'=1:5, 'b'=6:10, 'c'=11:15)
+      # Check which symmetry it has
+      cur.s <- names(all.n)[sapply(all.n, function(li){full.exp[.N,v] %in% li})]
+      # Sample other cluster as goal cluster
+      goal.s <- sample(names(all.n)[which(names(all.n)!=cur.s)],1)
+      if(cur.T==1){
+        # Sample goal
+        goal.v <- sample(idmap.d[[goal.s]],1)
+        # Sample full trajectory
+        v <- gen.D.Ham(full.exp[.N,v], goal.v, minRW, maxRW, idmap.d, idmap.dg2, c.map, idmap.bt, Edges)
+        # Append telomeres
+        all.telomeres <- rbind(all.telomeres, data.table(pp=ppn, s.type='test', g.type='deep ham', clust.loc='test', v=v[1], goal=v[length(v)],sym.id='ham'))
+      }else if(cur.T==2){
+        # Sample goal
+        goal.v <- sample(idmap.d[[goal.s]],1)
+        # Sample full trajectory
+        v <- gen.D.Min(full.exp[.N,v], goal.v, minRW, maxRW, idmap.d, idmap.dg2, c.map, idmap.bt, Edges)
+        # Append telomeres
+        all.telomeres <- rbind(all.telomeres, data.table(pp=ppn, s.type='test', g.type='deep min', clust.loc='test', v=v[1], goal=v[length(v)],sym.id='min'))
+      }else if(cur.T==3){
+        # Sample goal
+        goal.v <- sample(idmap.bt[[goal.s]],1)
+        # Sample full trajectory
+        v <- gen.B.Min(full.exp[.N,v], goal.v, minRW, maxRW, idmap.d, idmap.bg5,idmap.bg6, gsym5, gsym6, c.map, idmap.bt, Edges)
+        # Append telomeres
+        all.telomeres <- rbind(all.telomeres, data.table(pp=ppn, s.type='test', g.type='btn min', clust.loc='test', v=v[1], goal=v[length(v)],sym.id='min'))
+      }
+      # Append actual trajectory
+      full.exp <- rbind(full.exp, data.table(pp=ppn, miniblock=full.exp[.N,miniblock+1], v=v, nSteps=(length(v)-1):0, goal=v[length(v)]))
+      test.tally[cur.T] <- test.tally[cur.T]-1
+    }
+  }
+  return(list(full.exp[-1,], all.telomeres[-1,]))
 }
 
 
